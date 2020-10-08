31454ace4e98024a2d05890cb3e61d792364620b
frayn <yunfei.guo@hotmail.com>
2020-10-08 10:00:01 +0800

    addg2

---
+++ WordNode.java
@@ -0,0 +1,16 @@
+public class WordNode {
+    private int location;
+    private String word;
+    public WordNode(int location, String str) {
+        super();
+        this.location = location;
+        this.word = str;
+    }
+    public int getLocation() {
+        return location;
+    }
+    public String getWord() {
+        return word;
+    }
+
+}
